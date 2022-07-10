544109f90e4e88f1f0228d67c5d8e9725087d7da
sasikumargith <105166924+sasikumargith@users.noreply.github.com>
2022-07-10 07:59:15 +0530

    first commit

---
+++ Whatsappdevops/src/main/java/com/example/demo/WhatsappdevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappdevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsappdevopsApplication.class, args);
+	}
+
+}
